7b669ab63509abface37917576085db8f46b3e7c
Viktoria Szabo <viktoriaszabo@Viktoria-MacBook.local>
2018-08-25 12:23:12 +0200

    Első oldalam

---
+++ script.js
@@ -0,0 +1,19 @@
+let valtozo = 0;
+document.getElementById("ment").addEventListener("click", function (){ 
+    let title = document.getElementById("blogcim").value;
+    let text = document.getElementById("blogleiras").value;
+    
+    let dataToSave = {
+        title: title,
+        text: text
+    };
+     
+    fb.ref("posztok").once('value').then(data => {
+        
+    let savedPosts = data.val();
+    let path="posztok/"+savedPosts.length;
+    fb.ref(path).set(dataToSave);
+    savedPosts.length++; 
+    });
+    
+});
