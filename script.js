97a51a002e77d5cdb17a2c096b4c5e0ce5822177
Viktoria Szabo <viktoriaszabo@Viktoria-MacBook.local>
2018-08-25 15:19:16 +0200

    hozzáadtam gombfunkciókat

--- script.js
+++ script.js
@@ -16,5 +16,37 @@ document.getElementById("ment").addEventListener("click", function (){
     savedPosts.length++; 
     });
 
-    
 });
+
+document.getElementById("registration").addEventListener("click", function (){ 
+    alert("Itt majd lesz egy regisztrációs űrlap. :)")
+     });
+
+document.getElementById("forgetpw").addEventListener("click", function (){ 
+   alert("Ez a funkció még nem működik egyelőre. :D")
+    });
+
+
+    document.getElementById("bejelentkezes").addEventListener("click", function (){ 
+        let email = document.getElementById("emailcim").value;
+        let password = document.getElementById("jelszo").value;
+        
+        let dataToSave = {
+            email_cim: email,
+            jelszo: password
+        };
+         
+        fb.ref("bejelentkezes").once('value').then(data => {
+            
+        let savedPosts = data.val();
+        let path="bejelentkezes/"+savedPosts.length;
+        fb.ref(path).set(dataToSave);
+        savedPosts.length++; 
+        });
+    
+    });
+
+    let d = new Date();
+    document.getElementById("day").innerHTML = d.getDate();
+    document.getElementById("month").innerHTML = d.getMonth();
+    document.getElementById("year").innerHTML = d.getFullYear();
